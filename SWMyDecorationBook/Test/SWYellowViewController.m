d8f9aab517a6a24bb53b855eac43d0546d43659b
Eren <Eren.Shi@nextlabs.com>
2017-09-25 18:26:58 +0800

    import masony

--- SWMyDecorationBook/Test/SWYellowViewController.m
+++ SWMyDecorationBook/Test/SWYellowViewController.m
@@ -13,6 +13,7 @@
 #import "AppDelegate.h"
 @interface SWYellowViewController ()
 @property(nonatomic, assign) NSInteger index;
+@property(nonatomic, strong) UIButton *changeButton;
 @end
 
 @implementation SWYellowViewController
@@ -20,16 +21,27 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     self.view.backgroundColor = [UIColor yellowColor];
-    UIButton *changeButton = [[UIButton alloc] initWithFrame:CGRectMake(100, 100, 180, 180)];
+    UIButton *changeButton = [[UIButton alloc] initWithFrame:CGRectMake(10, 100, 180, 180)];
     [changeButton setTitle:@"Change View" forState:UIControlStateNormal];
     [changeButton addTarget:self action:@selector(changeButtonClicked:) forControlEvents:UIControlEventTouchUpInside];
+    changeButton.backgroundColor = [UIColor grayColor];
     [self.view addSubview:changeButton];
-    
+
+    _changeButton = changeButton;
 }
 
 - (void)changeButtonClicked:(UIButton *)button
 {
-    AppDelegate *appDelegate = (AppDelegate *)([UIApplication sharedApplication].delegate);
+     AppDelegate *appDelegate = (AppDelegate *)([UIApplication sharedApplication].delegate);
+    UIView *rootView = _changeButton;
+    while (rootView.superview) {
+        rootView = rootView.superview;
+    }
+    
+//    [rootView insertSubview:_changeButton aboveSubview:appDelegate.drawerVC.view];
+//    _changeButton.frame = CGRectMake(CGRectGetMidX(_changeButton.superview.frame), CGRectGetMidY(_changeButton.superview.frame), _changeButton.frame.size.width, _changeButton.frame.size.height);
+    return;
+   
     if (_index % 2) {
         SWRedViewController *redVC = [[SWRedViewController alloc] init];
         [appDelegate.drawerVC setCenterViewController:redVC withCloseAnimation:YES completion:nil];

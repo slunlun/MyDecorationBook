55baa15ba658b28a8cf0b38434f427323048bb50
Eren <Eren.Shi@nextlabs.com>
2018-01-23 10:54:23 +0800

    add initWithMO interface in logic model

--- SWMyDecorationBook/SWMarketCategory.h
+++ SWMyDecorationBook/SWMarketCategory.h
@@ -8,9 +8,13 @@
 
 #import <Foundation/Foundation.h>
 #import <UIKit/UIKit.h>
+#import "SWShoppingCategory+CoreDataClass.h"
+
 @interface SWMarketCategory : NSObject
+- (instancetype)initWithMO:(SWShoppingCategory *)category;
 @property(nonatomic, strong) NSString *categoryName;
 @property(nonatomic, strong) UIImage *categoryImage;
-@property(nonatomic, strong) NSArray *categoryItems;  // 当前分类下的商家列表
+@property(nonatomic, strong) NSMutableArray *categoryItems;  // 当前分类下的商家列表
 @property(nonatomic, assign) NSInteger indexNum;    // 用户排序
+@property(nonatomic, strong) NSDate *createTime;
 @end
--- SWMyDecorationBook/SWMarketCategory.m
+++ SWMyDecorationBook/SWMarketCategory.m
@@ -7,7 +7,36 @@
 //
 
 #import "SWMarketCategory.h"
-
+#import "SWShop+CoreDataClass.h"
+#import "SWMarketItem.h"
 @implementation SWMarketCategory
+- (instancetype)initWithMO:(SWShoppingCategory *)category {
+    if (self = [super init]) {
+//        @property(nonatomic, strong) NSString *categoryName;
+//        @property(nonatomic, strong) UIImage *categoryImage;
+//        @property(nonatomic, strong) NSArray *categoryItems;  // 当前分类下的商家列表
+//        @property(nonatomic, assign) NSInteger indexNum;    // 用户排序
+        _categoryName = category.name;
+        _indexNum = category.arrangeIndex;
+        _createTime = category.createTime;
+        NSSortDescriptor *sortDescriptor = [[NSSortDescriptor alloc] initWithKey:@"createTime" ascending:YES];
+        NSArray *sortDescriptors = @[sortDescriptor];
+        NSArray *categoryShops = [category.shops sortedArrayUsingDescriptors:sortDescriptors];
+        
+        for (SWShop *shop in categoryShops) {
+            SWMarketItem *market = [[SWMarketItem alloc] initWithMO:shop];
+            [self.categoryItems addObject:market];
+        }
+        
+    }
+    return self;
+}
 
+#pragma mark - Setter/Getter
+- (NSMutableArray *)categoryItems {
+    if (_categoryItems == nil) {
+        _categoryItems = [[NSMutableArray alloc] init];
+    }
+    return _categoryItems;
+}
 @end

ef482c65c27abfc6a935f689c6efb2ece57e814a
Eren Shi <eren.shi@nextlabs.com>
2018-08-03 11:36:04 +0800

    更新piechart的实现，不要在drawRect中画图

--- SWMyDecorationBook/SWPieChat.m
+++ SWMyDecorationBook/SWPieChat.m
@@ -34,7 +34,7 @@
 
 @implementation SWPieChat
 
-- (void)drawRect:(CGRect)rect {
+- (void)showPieChart {
     // 绘制圆环背景
     CGFloat sideLength = MIN(self.bounds.size.width, self.bounds.size.height);
     CGRect outRect = CGRectMake(5, 5, sideLength - 10, sideLength - 10);
@@ -59,7 +59,7 @@
     [self.layer addSublayer:bkLayer];
     self.bkLayer = bkLayer;
     
-   
+    
     for (SWPieChatSegment *segment in self.proportions) {
         UIBezierPath *path = [UIBezierPath bezierPath];
         [path moveToPoint:CGPointZero];
@@ -76,7 +76,7 @@
     }
     
     
-
+    
     // 添加总额的圆圈
     CALayer *countBackgourLayer = [CALayer layer];
     countBackgourLayer.bounds = CGRectMake(0, 0, 120, 120);
@@ -98,7 +98,7 @@
     countTextLayer.alignmentMode = kCAAlignmentCenter;
     [self.layer addSublayer:countBackgourLayer];
     [self.layer addSublayer:countTextLayer];
-   
+    
     
     // 添加边缘半透明的圆环
     UIBezierPath *ringPath = [UIBezierPath bezierPathWithArcCenter:pieCenter radius:radius - 4 startAngle:0 endAngle:2 * M_PI clockwise:YES];
@@ -134,7 +134,6 @@
     animation.timingFunction = [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionEaseInEaseOut];
     [animation setValue:@"strokeEnd" forKey:@"AnimationKey"];
     [maskLayer addAnimation:animation forKey:@"strokeEnd"];
-    
 }
 #pragma mark - Public interface / Update UI
 - (void)updateProportions:(NSArray<SWPieChatSegment *>*)proportions {
@@ -149,50 +148,50 @@
     }
     
     if (self.proportions) {
-        [self setNeedsDisplay];
+        [self showPieChart];
     }
 }
 
 #pragma mark - Gesture Recognizer
 - (void)touchesMoved:(NSSet *)touches withEvent:(UIEvent *)event{
-   
-       UITouch *touch = [touches anyObject];
     
-        NSUInteger toucheNum = [[event allTouches] count];//有几个手指触摸屏幕
-        if ( toucheNum > 1 ) {
-            return;//多个手指不执行旋转
-        }
-        
-        //_view，你想旋转的视图
-        if (![touch.view isEqual:self]) {
-            return;
-        }
-        
-        /**
-         CGRectGetHeight 返回控件本身的高度
-         CGRectGetMinY 返回控件顶部的坐标
-         CGRectGetMaxY 返回控件底部的坐标
-         CGRectGetMinX 返回控件左边的坐标
-         CGRectGetMaxX 返回控件右边的坐标
-         CGRectGetMidX 表示得到一个frame中心点的X坐标
-         CGRectGetMidY 表示得到一个frame中心点的Y坐标
-         */
-        
-        CGPoint center = self.pieCenter;
-        CGPoint currentPoint = [touch locationInView:touch.view];//当前手指的坐标
-        CGPoint previousPoint = [touch previousLocationInView:touch.view];//上一个坐标
-        
-        /**
-         求得每次手指移动变化的角度
-         atan2f 是求反正切函数 参考:http://blog.csdn.net/chinabinlang/article/details/6802686
-         */
-        CGFloat angle = atan2f(currentPoint.y - center.y, currentPoint.x - center.x) - atan2f(previousPoint.y - center.y, previousPoint.x - center.x);
-   
-        self.bkLayer.affineTransform = CGAffineTransformRotate(self.bkLayer.affineTransform, angle);
+    UITouch *touch = [touches anyObject];
+    
+    NSUInteger toucheNum = [[event allTouches] count];//有几个手指触摸屏幕
+    if ( toucheNum > 1 ) {
+        return;//多个手指不执行旋转
+    }
+    
+    //_view，你想旋转的视图
+    if (![touch.view isEqual:self]) {
+        return;
+    }
+    
+    /**
+     CGRectGetHeight 返回控件本身的高度
+     CGRectGetMinY 返回控件顶部的坐标
+     CGRectGetMaxY 返回控件底部的坐标
+     CGRectGetMinX 返回控件左边的坐标
+     CGRectGetMaxX 返回控件右边的坐标
+     CGRectGetMidX 表示得到一个frame中心点的X坐标
+     CGRectGetMidY 表示得到一个frame中心点的Y坐标
+     */
+    
+    CGPoint center = self.pieCenter;
+    CGPoint currentPoint = [touch locationInView:touch.view];//当前手指的坐标
+    CGPoint previousPoint = [touch previousLocationInView:touch.view];//上一个坐标
+    
+    /**
+     求得每次手指移动变化的角度
+     atan2f 是求反正切函数 参考:http://blog.csdn.net/chinabinlang/article/details/6802686
+     */
+    CGFloat angle = atan2f(currentPoint.y - center.y, currentPoint.x - center.x) - atan2f(previousPoint.y - center.y, previousPoint.x - center.x);
+    
+    self.bkLayer.affineTransform = CGAffineTransformRotate(self.bkLayer.affineTransform, angle);
 }
 
 - (void)touchesEnded:(NSSet<UITouch *> *)touches withEvent:(nullable UIEvent *)event {
-
+    
     CGAffineTransform layerTransform = CGAffineTransformInvert(self.bkLayer.affineTransform);
     // 根据当前的变换矩阵，反向变换 testPoint，使得testPoint仍然在初始坐标系下的(0, 20)点
     CGPoint testPoint = CGPointApplyAffineTransform(CGPointMake(0, 20), layerTransform);
@@ -200,7 +199,7 @@
         if (CGPathContainsPoint(segment.segmentLayer.path, nil, testPoint, false)) {
             CGFloat slideAngel = M_PI_2 - segment.centerAngle;
             self.bkLayer.affineTransform = CGAffineTransformRotate(CGAffineTransformIdentity, slideAngel);
-        
+            
             if ([self.delegate respondsToSelector:@selector(pieChat:didSelectSegment:)]) {
                 [self.delegate pieChat:self didSelectSegment:segment];
             }
@@ -234,9 +233,10 @@
                     }
                 }
             });
-          
+            
         }
     }
 }
 
 @end
+
